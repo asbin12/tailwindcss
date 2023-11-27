3dd6540be69481494311c4fcc7539b2d7420b3f8
asbin12 <140784814+asbin12@users.noreply.github.com>
2023-11-27 08:27:41 +0545

    Some part is complete some part remaining

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
       },
       backgroundImage: {
         "section-4": "url('assets/image/section-4-bg.png')",
-        
+      },
+      fontSize: {
+        "4.5xl": ["44px", "52px"],
+        "3.5xl": ["32px", "40px"],
       },
     },
   },
