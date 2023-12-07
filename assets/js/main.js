734e9542c26aa92d77d66e3e3a8ee901c5441692
asbin12 <140784814+asbin12@users.noreply.github.com>
2023-12-07 09:23:13 +0545

    overall code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,55 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2.5,
-  // spaceBetween: 30,
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 2.5,
+//   freeMode: true,
+//   pagination: {
+//     el: ".swiper-pagination1",
+//     type: "progressbar",
+//   },
+//   mousewheel: true,
+// });
+
+// var swiper = new Swiper(".mySecondSwiper", {
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   // mousewheel: true,
+// });
+
+// var swiper = new Swiper(".myTestimonialSwiper", {
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   mousewheel: true,
+//   keyboard: true,
+// });
+
+// const navLinks = document.querySelector(".nav-links");
+// function onToggleMenu(e) {
+//   e.name = e.name === "menu" ? "close" : "menu";
+//   navLinks.classList.toggle("top-[100%]");
+//   // console.log(e.name);
+// }
+var swiperOptions = {
   freeMode: true,
-  // centeredSlides: true,
   pagination: {
     el: ".swiper-pagination1",
-    clickable: true,
+    type: "progressbar",
   },
   mousewheel: true,
-});
+};
+
+if (window.innerWidth >= 1024 && window.innerWidth < 1280) {
+  swiperOptions.slidesPerView = 2;
+} else if (window.innerWidth >= 1280) {
+  swiperOptions.slidesPerView = 2.5;
+}
+
+var swiper = new Swiper(".mySwiper", swiperOptions);
 
 var swiper = new Swiper(".mySecondSwiper", {
   spaceBetween: 30,
@@ -29,3 +70,10 @@ var swiper = new Swiper(".myTestimonialSwiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+const navLinks = document.querySelector(".nav-links");
+function onToggleMenu(e) {
+  e.name = e.name === "menu" ? "close" : "menu";
+  navLinks.classList.toggle("top-[100%]");
+  // console.log(e.name);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      screens: {
+        sm: "640px",
+
+        md: "768px",
+
+        lg: "1024px",
+
+        xl: "1280px",
+
+        "2xl": "1536px",
+      },
       fontFamily: {
         primary: ["Manrope"],
         outfit: ["Outfit"],
