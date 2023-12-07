c8234c2443762fe1f8f1bcb180d8b387d744d277
asbin12 <140784814+asbin12@users.noreply.github.com>
2023-12-07 12:08:01 +0545

    bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,3 @@
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 2.5,
-//   freeMode: true,
-//   pagination: {
-//     el: ".swiper-pagination1",
-//     type: "progressbar",
-//   },
-//   mousewheel: true,
-// });
-
-// var swiper = new Swiper(".mySecondSwiper", {
-//   spaceBetween: 30,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   // mousewheel: true,
-// });
-
-// var swiper = new Swiper(".myTestimonialSwiper", {
-//   cssMode: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   mousewheel: true,
-//   keyboard: true,
-// });
-
-// const navLinks = document.querySelector(".nav-links");
-// function onToggleMenu(e) {
-//   e.name = e.name === "menu" ? "close" : "menu";
-//   navLinks.classList.toggle("top-[100%]");
-//   // console.log(e.name);
-// }
 var swiperOptions = {
   freeMode: true,
   pagination: {
@@ -71,9 +35,4 @@ var swiper = new Swiper(".myTestimonialSwiper", {
   keyboard: true,
 });
 
-const navLinks = document.querySelector(".nav-links");
-function onToggleMenu(e) {
-  e.name = e.name === "menu" ? "close" : "menu";
-  navLinks.classList.toggle("top-[100%]");
-  // console.log(e.name);
-}
+
